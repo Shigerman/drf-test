4b8ecb450b8da633b0848e3337935bbc9d2f950c
Larisa Shigareva <thelarisa81@gmail.com>
2021-04-02 22:15:55 +0300

    DRF tutorial starting point

--- backend/settings.py
+++ backend/settings.py
@@ -45,7 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'backend.app'
+    'rest_framework',
+
+    'backend.app',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
